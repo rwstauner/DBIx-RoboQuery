53399c963cdab2f68fd46dc635a73dd403ba1afd
Randy Stauner <randy@magnificent-tears.com>
2010-11-20 23:53:29 -0700

    Create (empty) main module for documentation

---
+++ lib/DBIx/Enabler.pm
@@ -0,0 +1,22 @@
+package DBIx::Enabler;
+# ABSTRACT: Enable yourself to do more with DBI
+
+1;
+
+=head1 DESCRIPTION
+
+Currently this module is just a namespace.
+
+The modules in this namespace are meant to enable
+the user to do more than basic DBI programming.
+
+This means getting more information
+and doing more powerful things.
+
+=head1 SEE ALSO
+
+=for :list
+* L<DBIx::Enabler::Query>
+* L<DBIx::Enabler::ResultSet>
+
+=cut
