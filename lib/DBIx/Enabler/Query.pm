86ac9e6d3b63a8fae64b132e0ddf97bc87f58eb7
Randy Stauner <randy@magnificent-tears.com>
2010-11-22 21:07:52 -0700

    Change constructor to accept solely a hash or hashref
    Explicitly specify 'sql' or 'file'
    instead of knowing the difference b/t a string and a scalar ref.
    
    Accept hash list because I like it.
    perlmodstyle says I can.

--- lib/DBIx/Enabler/Query.pm
+++ lib/DBIx/Enabler/Query.pm
@@ -3,8 +3,9 @@ package DBIx::Enabler::Query;
 
 =head1 SYNOPSIS
 
-	DBIx::Enabler::Query->new(\"SELECT * FROM table");
-	DBIx::Enabler::Query->new("/path/to/query.sql", {variables => {}});
+	DBIx::Enabler::Query->new(sql => "SELECT * FROM table");
+	DBIx::Enabler::Query->new(file => "/path/to/query.sql", variables => {});
+	DBIx::Enabler::Query->new({sql => \"SELECT * FROM table", variables => {}});
 
 An object to encapsulate a database query
 and various methods to provide you with more information
@@ -15,6 +16,8 @@ and configuration of your queries.
 use strict;
 use warnings;
 
+use Carp qw(carp croak);
+use DBIx::Enabler ();
 use Template 2.22; # Template Toolkit
 
 =method new
@@ -25,18 +28,52 @@ First argument is the sql template to process.
 * A string is treated as a filename,
 * A scalar reference is treated as the template text.
 
-The I<variables> attribute can be set to a hashref
-of variables made avaiable to the template.
+The second argument is a hash or hashref of options:
+
+=for :list
+* I<sql>
+The SQL query [template] in a string (or a reference to a string)
+* I<file>
+The file path of a SQL query [template] (mutually exclusive with I<sql>)
+* I<variables>
+A hashref of variables made available to the template
 
 =cut
 
+my @pass_through_args = qw(
+	variables
+);
+
 sub new {
 	my $class = shift;
+	my %opts = ref($_[0]) eq 'HASH' ? %{$_[0]} : @_;
+
+	# Params::Validate not currently warranted
+	# (since it's still missing the "mutually exclusive" feature)
+
+	# defaults
 	my $self = {
 		variables => {},
-		template => shift,
-		@_ ? %{$_[0]} : ()
 	};
+
+	foreach my $var ( @pass_through_args ){
+		$self->{$var} = $opts{$var} if exists($opts{$var});
+	}
+
+	croak(q|Cannot include both 'sql' and 'file'|)
+		if exists($opts{sql}) && exists($opts{file});
+
+	# if the string is defined that's good enough
+	if( defined($opts{sql}) ){
+		$self->{template} = ref($opts{sql}) ? ${$opts{sql}} : $opts{sql};
+	}
+	# the file path should at least be a true value
+	elsif( my $f = $opts{file} ){
+		$self->{template} = DBIx::Enabler::slurp_file($f);
+	}
+	else {
+		croak(q|Must specify one of 'sql' or 'file'|);
+	}
 
 	$self->{tt} = Template->new(
 		ABSOLUTE => 1,
@@ -64,7 +101,7 @@ sub sql {
 	my ($self, $vars) = @_;
 	$vars ||= {};
 	my $output;
-	$self->{tt}->process($self->{template}, $vars, \$output)
+	$self->{tt}->process(\$self->{template}, $vars, \$output)
 		or die($self->{tt}->error(), "\n");
 	return $output;
 }
